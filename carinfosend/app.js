557f1848b6e56a8f6b4dd2577cd00d9f41d17f6a
MAllstar <46458404+MAllstar@users.noreply.github.com>
2019-12-22 18:45:09 +0800

    Update app.js

--- carinfosend/app.js
+++ carinfosend/app.js
@@ -20,11 +20,14 @@ app.use('/carMeneger', require('./routes/carMeneger'));
 app.use('/msgSend', require('./routes/msgSend'));
 
 
-app.set('views', path.join(__dirname, 'views'));
-//静态资源
+//静态资源目录
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('view engine', 'hbs');//设置模板引擎
+//绑定模板路径
+app.set('views', path.join(__dirname, 'views'));
+//设置模板引擎
+app.set('view engine', 'hbs');
+//模板引擎配置
 app.engine('.hbs', hbs ({
     extname: '.hbs',
     defaultLayout: 'default',
@@ -32,4 +35,21 @@ app.engine('.hbs', hbs ({
     partialsDir: __dirname + '/views/partials',
   }));
 
-app.listen(3001)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error', {layout: false, status: 500});   //取消自动加载的默认布局模板
+});
+
+app.listen(3001);
