068d75cb32a510488bf9f56625538c60f496ccbf
MAllstar <1160809932@qq.com>
2020-05-13 15:58:15 +0800

    delete old version

--- carinfosend/routes/msgSend.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/',  function(req, res,next){
-    res.render('generary/msgSend',{title:"信息分发"});
-  });
-
-module.exports = router;
